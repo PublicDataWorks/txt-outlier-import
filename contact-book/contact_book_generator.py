7e57de291764760b96955039fb984d07337f9624
Dat Tran + Ky Phan <ky.phan@eastagile.com>
2023-12-14 17:50:45 +0700

    [f] Only import user phone number
    [OUT-3](https://linear.app/pdw/issue/OUT-3/import-contacts-and-contact-metadata)
    
    Co-authored-by: Dat Tran <dat.h.tran@eastagile.com>

--- contact-book/contact_book_generator.py
+++ contact-book/contact_book_generator.py
@@ -9,42 +9,25 @@ db_url = os.environ["DB_URL"]
 engine = create_engine(db_url)
 
 query = """
-SELECT
-    'Users' as "Group Membership",
-    'Main' as "Phone 1 - Type",
-    phone as "Phone 1 - Value",
-    MAX(first_name) as "Given Name",
-    MAX(last_name) as "Family Name"
-FROM (
-    SELECT
-        phone,
-        NULL AS first_name,
-        NULL AS last_name
-    FROM user_locations ul
-    RIGHT JOIN (
-        SELECT *
-        FROM user_phones up
-        WHERE phone NOT IN (
-            SELECT DISTINCT phone
-            FROM sms_messages sm
-            JOIN sms_threads st ON sm."thread_id" = st."id"
-            WHERE TRIM(LOWER(message_text)) ILIKE
-            ANY (ARRAY['UNSUBSCRIBE', 'END', 'CANCEL', 'QUIT', 'STOPALL', 'STOP'])
-        ) AND sms_enabled = true
-    ) AS sub ON ul.id = sub.id
-    UNION (
-        SELECT phone, first_name, last_name
-        FROM user_master um
-        WHERE phone NOT IN (
-            SELECT phone FROM user_master_unsubscribes
-        )
+SELECT 'Users' as "Group Membership", 'Main' as "Phone 1 - Type", phone as "Phone 1 - Value"
+FROM user_phones
+WHERE phone NOT IN (
+    SELECT phone
+    FROM sms_threads st
+    JOIN sms_messages sm ON sm.thread_id = st.id
+    WHERE TRIM(LOWER(message_text)) ILIKE ANY (ARRAY['UNSUBSCRIBE', 'END', 'CANCEL', 'QUIT', 'STOPALL', 'STOP'])
+) AND sms_enabled = true
+UNION (
+    SELECT 'Users' as "Group Membership", 'Main' as "Phone 1 - Type", phone as "Phone 1 - Value"
+    FROM user_master
+    WHERE phone NOT IN (
+        SELECT phone FROM user_master_unsubscribes
     )
-) AS combined
-GROUP BY phone
+)
 """
 
 df_result = pd.read_sql_query(query, engine)
-no_rows_per_file = 25_000
+no_rows_per_file = 40_000
 n_files = math.ceil(df_result.shape[0] / no_rows_per_file)
 for i in range(n_files):
     df_temp = df_result.iloc[i * no_rows_per_file : (i + 1) * no_rows_per_file]
